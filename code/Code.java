f4d1ef05937d7e82f6ac569a900774a21037fb39
bruceeifler@gmail.com <bruceeifler@gmail.com>
2024-10-04 19:00:41 -0600

    *

--- code/Code.java
+++ code/Code.java
@@ -4,12 +4,13 @@ import java.nio.charset.StandardCharsets;
 
 public class Code {
     char byteMap[][];
+    char encodedMap[][];
     int alphaSize =36;
     char alphaMap[][]=new char[alphaSize][alphaSize];
     String alphaMapFileName="AlphaMap.txt";
     int cyclicKeyIndex=0;
     String cyclicKey="U90N4BWUP7W174NU91Y571CK";
-    char[] baseSet = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    char[] baseSet = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
     // Implementing Fisher–Yates shuffle
     char [] getShuffleCharArray()
@@ -201,6 +202,8 @@ public class Code {
         myCode.setupAlphaMap();
 
         myCode.printMap(myCode.alphaMap);
+
+    //    myCode.encodedMap=myCode.encodeMap(myCode.byteMap);
 
     }
 }
