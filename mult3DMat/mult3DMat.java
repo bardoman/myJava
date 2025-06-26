69a8f991017594c824c7d602b4a8e624bfc9aa25
bardoman <bruceeifler@gmail.com>
2025-06-25 20:56:15 -0600

    *

--- mult3DMat/mult3DMat.java
+++ mult3DMat/mult3DMat.java
@@ -17,6 +17,8 @@ int[][][] matD;
 
 matA=rand3DMat(3);
 matB=rand3DMat(3);
+
+System.out.println("mult3DMat:\n");
      print3DMat(matA);
      print3DMat(matB);
 
@@ -39,37 +41,29 @@ matB=rand3DMat(3);
    for (int x = 0; x < size; x++) { 
             for (int y = 0; y < size; y++) { 
                 for (int z = 0; z < size; z++) { 
-                    tmp[x][y][z]=random.nextInt(100);
+                    tmp[x][y][z]=random.nextInt(10);
                 }
             }
         }	
 	  return tmp;
   }
   
-  public static void print3DMat(int[][][] mat) {
-   int depth = mat.length;
-        int rowsA = mat[0].length;
-        int colsA = mat[0][0].length; // Also rowsB
-        int colsB = mat[0][0].length;
-        
-   for (int d = 0; d < depth; d++) { // Iterate through depth (slices)
-            for (int r = 0; r < rowsA; r++) { // Iterate through rows of current 2D slice in A
-                for (int c = 0; c < colsB; c++) { // Iterate through columns of current 2D slice in B
-                    for (int k = 0; k < colsA; k++) { // Perform dot product for current element
-                       // resultMatrix[d][r][c] += matrixA[d][r][k] * matrixB[d][k][c];
-                         System.out.print(mat[d][r][k]);
+   public static void print3DMat(int[][][] mat) {
+	  int size = mat.length;
+            
+   for (int x = 0; x < size; x++) { 
+            for (int y = 0; y < size; y++) { 
+                for (int z = 0; z < size; z++) { 
+                    System.out.print(mat[x][y][z]);
                          System.out.print(",");
-                    }
                 }
                 System.out.println();
             }
-        }	
-        System.out.println();  
-	  
+        }
+        System.out.println();	
   }
 
    public static int[][][] multiply3DMatrices(int[][][] matrixA, int[][][] matrixB) {
-        // Assume matrices are already checked for compatibility
         int depth = matrixA.length;
         int rowsA = matrixA[0].length;
         int colsA = matrixA[0][0].length; // Also rowsB
