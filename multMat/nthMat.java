bab490a7ca9eb6284bc9fd996febf0fb88b3cb97
bardoman <bruceeifler@gmail.com>
2025-07-06 11:29:46 -0600

    *

--- multMat/nthMat.java
+++ multMat/nthMat.java
@@ -1,8 +1,9 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+import java.util.*;
 
 public class nthMat {
-	public static int size=100;
+	public static int size=4;
 	public static Integer[]intArray = new Integer[size]; //{0,1, 2, 3, 4, 5,6,7,8,9};
 
     public static void main(String[] args) {
@@ -16,14 +17,43 @@ public class nthMat {
 			
         System.out.println("********base:"); 
         System.out.println(base); 
+
+//*********************************************************
         
         ArrayList<ArrayList> base2 = new ArrayList<>();
-        initListRecurs(base2,size);
-        
-        System.out.println("********base2:"); 
+        alocListRecurs(base2,10);
+                
+        System.out.println("****alocListRecurs  ****base2:"); 
         System.out.println(base2);
+        
+        System.out.println("********traverse base2:"); 
+        
+    //    traverseArrayList(base2);
+    }
+ 
+    public static void traverseArrayList(List<?> list){
+        for (Object element : list) {
+            if (element instanceof List<?>) {
+                // If it's a List, recursively traverse it
+                traverseArrayList((List<?>) element);
+            } else {
+                // If it's not a List, process the element
+                System.out.println(element);
+            }
+        }
     }
     
+/*    public static void initList(ArrayList<ArrayList> list)
+    {
+		Iterator<Integer> iterator = list.iterator();
+    while (iterator.hasNext()) {
+        ArrayList subList = iterator.next();
+        
+    }
+		
+	}
+*/	
+	
     public static void initIntArray(Integer[] ary)
     {
 		int size = ary.length;
@@ -34,15 +64,16 @@ public class nthMat {
 		}		
 	}
 		  
-     public static void initListRecurs(ArrayList<ArrayList> list, int count) {
+     public static void alocListRecurs(ArrayList<ArrayList> list, int count) {
             if (count == 0) {
                 return;
             }
-            ArrayList tmp=new ArrayList<>(Arrays.asList(intArray));
-            
-            list.add(tmp);
 
-            initListRecurs(list, count - 1);
+           ArrayList tmp=new ArrayList<Integer>(Arrays.asList(intArray));
+		
+           list.add(tmp);
+           
+            alocListRecurs(list, count - 1);
         }     
  /*
      public static int[][][][]  multDMat(ArrayList<ArrayList> matA, ArrayList<ArrayList> matB) {
